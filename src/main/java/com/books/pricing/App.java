465be302fd3f29aa0780f1b33242294a93559daa
Vivek Sharma <viveksharma.svim@gmail.com>
2016-08-30 15:57:35 +0530

    Complete all assignment .

---
+++ src/main/java/com/books/pricing/App.java
@@ -0,0 +1,13 @@
+package com.books.pricing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
